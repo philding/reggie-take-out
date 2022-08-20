80ca62554ec113bf9814bdbd6583e7e1166d9d5d
qfDing <81175308>
2022-08-20 17:22:02 +0800

    Test

---
+++ src/main/java/com/dqf/config/TestConfig.java
@@ -0,0 +1,15 @@
+package com.dqf.config;
+
+import java.io.Serializable;
+
+/**
+ * @author qf.Ding
+ * @date 2022/8/20 14:05
+ */
+public class TestConfig implements Serializable {
+    private Integer integer;
+    public void test() {
+
+
+    }
+}
