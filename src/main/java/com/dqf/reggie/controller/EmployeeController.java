a413099b261557d6df051c4649ee88d6cf57c1e3
qfDing <81175308>
2022-08-21 22:24:13 +0800

    登录功能开发

--- src/main/java/com/dqf/reggie/controller/EmployeeController.java
+++ src/main/java/com/dqf/reggie/controller/EmployeeController.java
@@ -1,10 +1,12 @@
 package com.dqf.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.dqf.reggie.common.R;
 import com.dqf.reggie.entity.Employee;
 import com.dqf.reggie.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,15 +33,39 @@ public class EmployeeController {
     @PostMapping("/login")
     public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee) {
         /**
-         *
+         * 将页面提交密码进行加密处理
          */
+        String password =employee.getPassword();
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
 
-        employee.getPassword();
+        /**
+         * 根据页面提交的用户名进行查询
+         */
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Employee::getUsername, employee.getUsername());
+        Employee emp = employeeService.getOne(queryWrapper);
 
+        if (emp == null) {
+            return R.error("登录失败");
+        }
 
+        /**
+         * 密码比对
+         */
+        if (!emp.getPassword().equals(password)) {
+            return R.error("密码错误");
 
-        return null;
+        }
 
+        if (emp.getStatus() == 0) {
+            return R.error("账号已经禁用");
+        }
+
+        /**
+         * 登陆成功,将员工ID写入session并且返回
+         */
+        request.getSession().setAttribute("employee", emp.getId());
+        return R.success(emp);
     }
 
 
